4c9bd423a011a3c4ae6f0dc0993b66766ac68a4a
soumyadeep9800 <152770412+soumyadeep9800@users.noreply.github.com>
2025-06-06 00:08:28 +0530

    school_Management

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.get('/', (req, res) => {
   res.send('Welcome to my School');
 });
 
+//add school
 app.post('/addSchool', async (req, res) => {
   const { name, address, latitude, longitude } = req.body;
 
@@ -63,6 +64,45 @@ app.post('/addSchool', async (req, res) => {
     console.error('Insert Error:', error.message);
     res.status(500).json({ error: error.message });
   }
+});
+
+//list of school
+app.get('/listSchools', async (req,res)=>{
+    const {latitude, longitude}=req.query;
+
+    if(!latitude || !longitude || isNaN(latitude) || isNaN(longitude)){
+        return res.status(400).json({error: "Invalid or missing latitude/longitude"});
+    }
+    
+    const userLat = parseFloat(latitude);
+    const userLon = parseFloat(longitude);
+
+    try {
+        const [schools] = await db.execute('SELECT * FROM schools');
+        //Haversine
+        const toRad = (value) => (value * Math.PI) / 180;
+        const calculateDistance = (lat1, lon1, lat2, lon2) => {
+        const R = 6371; // km
+        const dLat = toRad(lat2 - lat1);
+        const dLon = toRad(lon2 - lon1);
+        const a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
+        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        return R * c;
+        };
+        const schoolsWithDistance = schools.map(school => {
+        const distance = calculateDistance(userLat, userLon, school.latitude, school.longitude);
+        return { ...school, distance: parseFloat(distance.toFixed(2)) };
+        });
+
+        const sortedSchools = schoolsWithDistance.sort((a, b) => a.distance - b.distance);
+        res.json(sortedSchools);
+    } catch (error) {
+        console.error('Query Error:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
 });
 
 app.listen(port, () => {
